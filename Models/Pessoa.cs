2130798c9b21eb9c2612fb09f988b8db3bc97cc8
William Rodrigues <wllir12@hotmail.com>
2025-03-27 12:12:20 -0300

    Initial commit

---
+++ Models/Pessoa.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArvoreGenealogica.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public Pessoa Conjuge { get; set; }
+        public List<Pessoa> Filhos { get; set; } = new List<Pessoa>();
+
+        public Pessoa(string nome)
+        {
+            Nome = nome;
+        }
+
+        public Pessoa(string nome, Pessoa paiOuMae)
+        {
+            Nome = nome;
+            paiOuMae.AdicionarFilho(this);
+        }
+
+        public void AdicionarFilho(Pessoa filho)
+        {
+            Filhos.Add(filho);
+        }
+
+        public void Conjugue(Pessoa conjuge)
+        {
+            if (this.Conjuge == null)
+            {
+                this.Conjuge = conjuge;
+                conjuge.Conjuge = this;
+            }
+        }
+
+        public void ImprimeArvore(int nivel = 0)
+        {
+            string indentacao = new string(' ', nivel * 4);
+            string status = Conjuge != null ? $"é casado(a) com {Conjuge.Nome}" : "está solteiro(a)";
+            string filhosText = Filhos.Count > 0 ? " filhos:" : "";
+
+            Console.WriteLine($"{indentacao}-->{Nome} {status}{filhosText}");
+
+            foreach (var filho in Filhos)
+            {
+                filho.ImprimeArvore(nivel + 1);
+            }
+        }
+    }
+}
---
+++ TestaArvore.cs
@@ -0,0 +1,59 @@
+﻿using ArvoreGenealogica.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArvoreGenealogica
+{
+    class TestaArvore
+    {
+        static void Main(string[] args)
+        {
+            Pessoa Lily = new Pessoa("Lily");
+            Lily.Conjugue(new Pessoa("Wilhelm"));
+
+            // Patriarcas
+            Pessoa Opa = new Pessoa("Opa", Lily);
+            Opa.Conjugue(new Pessoa("Oma"));
+
+            // Filhos de Oma e Opa
+            Pessoa Reinhold = new Pessoa("Reinhold", Opa);
+            Pessoa Wilma = new Pessoa("Wilma", Opa);
+            Pessoa Sigrid = new Pessoa("Sigrid", Opa);
+
+            // Filhos e esposa de Reinhold
+            Reinhold.Conjugue(new Pessoa("Sonia"));
+            Pessoa Christian = new Pessoa("Christian", Reinhold);
+            Pessoa Gabrielle = new Pessoa("Gabrielle", Reinhold);
+            Pessoa Sabine = new Pessoa("Sabine", Reinhold);
+
+            // Filhos e marido de Wilma
+            Wilma.Conjugue(new Pessoa("Rodolfo"));
+            Pessoa Ricardo = new Pessoa("Ricardo", Wilma);
+            Pessoa Rodrigo = new Pessoa("Rodrigo", Wilma);
+
+            // Ricardo e esposa
+            Ricardo.Conjugue(new Pessoa("Debora"));
+
+            // Filhos e esposa de Christian
+            Christian.Conjugue(new Pessoa("Mônica"));
+            Pessoa Oscar = new Pessoa("Oscar", Christian);
+            Pessoa Lorena = new Pessoa("Lorena", Christian);
+
+            // Filhos e marido de Sigrid
+            Sigrid.Conjugue(new Pessoa("Peter"));
+            Pessoa Martin = new Pessoa("Martin", Sigrid);
+            Pessoa Thomas = new Pessoa("Thomas", Sigrid);
+            Pessoa Claudia = new Pessoa("Claudia", Sigrid);
+
+            // Martin e esposa
+            Martin.Conjugue(new Pessoa("Carla"));
+            Martin.AdicionarFilho(new Pessoa("Nicolas"));
+
+            // Impressão da árvore genealógica
+            Lily.ImprimeArvore(0);
+        }
+    }
+}
